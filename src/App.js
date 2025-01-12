c0d1c469508bd13c1032e4df24ce2c9cf3e5cefa
Amol Mahale <mahalea1000@gmail.com>
2025-01-12 12:51:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import Facebook from "./Components/Facebook.js"
+function App() {
+  return (
+    <Facebook/>
+  );
+}
+
+export default App;
---
+++ src/Components/Facebook.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./facebook.css";
+function Facebook(){
+
+    return(
+        <div className="mainContainer">
+            <h1>Facebook</h1>
+            <div className="container">
+                <span>Log in to Facebook</span>
+                <input type="text" placeholder="   Email address or phone number"/>
+                <input type="text" placeholder="   password"/>
+                <button>Log In</button>
+                <a href="#">Forgotten account?  Sign up for Facebook</a>
+            </div>
+        </div>
+    )
+}
+
+export default Facebook;
